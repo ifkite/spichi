7672b603d9e89e5c8aaf31a2fb084b31ef82243e
ifkite <holahello@163.com>
2017-08-15 11:41:22 +0800

    修改 异常处理

--- excepts.py
+++ excepts.py
@@ -12,9 +12,11 @@
 '''
 
 
+import json
+
 class SpichiExcept(Exception):
 
-    error_code = 500
+    status_code = 500
     error_msg = 'Internal Server Error'
 
 
@@ -24,53 +26,78 @@ class InternalServerExcept(SpichiExcept):
 
 class BadRequestExcept(SpichiExcept):
 
-    error_code = 400
+    status_code = 400
     error_msg = 'Bad Request'
 
 
 class UnauthorizedExcept(SpichiExcept):
 
-    error_code = 401
+    status_code = 401
     error_msg = 'Unauthorized'
 
 
 class PaymentRequiredExcept(SpichiExcept):
 
-    error_code = 402
+    status_code = 402
     error_msg = 'Payment Required'
 
 
 class ForbiddenExcept(SpichiExcept):
 
-    error_code = 403
+    status_code = 403
     error_msg = 'Forbidden'
 
 
 class NotFoundExcept(SpichiExcept):
 
-    error_code = 404
+    status_code = 404
     error_msg = ' Not Found'
 
 
 class MethodNotAllowedExcept(SpichiExcept):
 
-    error_code = 405
+    status_code = 405
     error_msg = 'Method Not Allowed'
 
 
 class NotAcceptableExcept(SpichiExcept):
 
-    error_code = 406
+    status_code = 406
     error_msg = 'Not Acceptable'
 
 
 class RequestTimeoutExcept(SpichiExcept):
 
-    error_code = 408
+    status_code = 408
     error_msg = 'Request Timeout'
 
 
 class GoneExcept(SpichiExcept):
 
-    error_code = 410
+    status_code = 410
     error_msg = 'Gone'
+
+
+class ExceptHandler(object):
+
+    def __init__(self, e):
+        from werkzeug.wrappers import Response
+
+        status = getattr(e, 'status_code', 400)
+        err_content = self.set_content(e)
+        self.response = Response(err_content, status=status, mimetype='application/json')
+
+    def set_content(self, e):
+        error_msg = getattr(e, 'error_msg', '')
+        error_detail = getattr(e, 'message', '')
+        error_code = getattr(e, 'error_code', 400)
+        return json.dumps({
+                    "error_code": error_code,
+                    "error_msg": error_msg,
+                    "error_detail": error_detail,
+                    "results": []
+                })
+
+
+def handle_except(e, handler):
+    return handler(e).response
--- spichi.py
+++ spichi.py
@@ -11,11 +11,10 @@
 '''
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
-from werkzeug.exceptions import HTTPException
 import json
 
 from session import SessionHandler
-from excepts import SpichiExcept
+from excepts import ExceptHandler, handle_except
 
 
 class Spichi(object):
@@ -53,25 +52,16 @@ class Spichi(object):
             handler().post_process(request, response)
 
     def response_wrapper(self, endpoints, request, values):
-        results = []
-        error_code = 200
-        error_msg = ''
-        try:
-            results = json.dumps(self.view_func[endpoints](request, **values))
-            return json.dumps({'error_code': error_code, 'error_msg': error_msg, 'results': results})
-        except SpichiExcept as e:
-            error_code = e.error_code
-            error_msg = e.error_msg
-            return json.dumps({'error_code': error_code, 'error_msg': error_msg, 'results': results})
-
+        results = self.view_func[endpoints](request, **values) or []
+        return json.dumps({'error_code': 200, 'error_msg': '', 'error_detail': '', 'results': results})
 
     def dispatch_response(self, request):
         adapter = self.url_map.bind_to_environ(request.environ)
         try:
             endpoints, values = adapter.match()
-            return Response(self.response_wrapper(endpoints=endpoints, request=request, values=values))
-        except HTTPException as e:
-            return e
+            return Response(self.response_wrapper(endpoints=endpoints, request=request, values=values), mimetype='application/json')
+        except Exception as e:
+            return handle_except(e, ExceptHandler)
 
     def wsgi_app(self, environ, start_response):
         request = Request(environ)
